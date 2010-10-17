ccbe9a6a09827eb130e0255d297fc0623a648c39
orbitz <orbitz@gmail.com>
2010-10-17 19:19:21 -0400

    Cleaning up code a bit

--- lib/bio_genbank.ml
+++ lib/bio_genbank.ml
@@ -1,4 +1,5 @@
 open Ort
+open Ort.Function
 
 let gene_re = Str.regexp ".*CDS +\\([0-9]+\\)\\.\\.\\([0-9]+\\)$"
 
@@ -7,18 +8,19 @@ let gene_re = Str.regexp ".*CDS +\\([0-9]+\\)\\.\\.\\([0-9]+\\)$"
  * this only loads a list of genes listed in the genbank file
  *)
 let read fname =
-  List.rev (Seq.fold
-	      ~f:(fun a l ->
-		if Str.string_match gene_re l 0 then
-		  let s = int_of_string (Str.matched_group 1 l) - 1 in
-		  let e = int_of_string (Str.matched_group 2 l) in
-		  if (e - s) mod 3 = 0 then
-		    (s, e)::a
-		  else
-		    a
-		else
-		  a)
-	      ~init:[]
-	      (Lazy_io.read_file_lines ~close:true (open_in fname)))
-
+  Lazy_io.read_file_lines ~close:true (open_in fname)
+  |> Seq.fold
+      ~f:(fun a l ->
+	if Str.string_match gene_re l 0 then
+	  let s = int_of_string (Str.matched_group 1 l) - 1 in
+	  let e = int_of_string (Str.matched_group 2 l) in
+	  if (e - s) mod 3 = 0 then
+	    (s, e)::a
+	  else
+	    a
+	else
+	  a)
+      ~init:[]
+  |> List.rev
+    
 	 
