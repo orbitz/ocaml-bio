ffb0cb877a7e9819eef05148e3e3d4dbb38d4669
orbitz <orbitz@gmail.com>
2012-08-26 14:18:32 +0200

    Fixing bio_newick for latest version of core

--- lib/bio_newick.ml
+++ lib/bio_newick.ml
@@ -40,7 +40,7 @@ let string_of_list l =
   l |> List.iter ~f:(Buffer.add_char b);
   Buffer.contents b
 
-let is_digit = List.mem ~set:['0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9']
+let is_digit = List.mem ['0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9']
 
 let rec newick = parser
   | [< _ = whitespace; ''('; _ = whitespace; t = tree; _ = whitespace; '')'; _ = whitespace; '';'; _ = whitespace >] -> Newick t
@@ -53,11 +53,11 @@ and entry_aux = parser
   | [< '','; _ = whitespace; e = entry; es = entry_aux >] -> e::es
   | [< >] -> []
 and identifier s = s |> Seq.take_while ~f:((<>) ':') |> Seq.to_list |> string_of_list
-and distance s = 
+and distance s =
   let floating_point c = is_digit c || c = '-' || c = '.' || c = 'e' in
   s |> Seq.take_while ~f:floating_point |> Seq.to_list |> string_of_list |> float_of_string
-and whitespace = 
-  let is_whitespace = List.mem ~set:[' '; '\t'; '\n'] in
+and whitespace =
+  let is_whitespace = List.mem [' '; '\t'; '\n'] in
   Seq.drop_while ~f:is_whitespace
 
 let parse s = newick s
